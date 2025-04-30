6bceaf29e707c8cce31c9affa6f81cec4ccbb7c8
manirajk29 <manirajkvm29@gmail.com>
2025-04-30 12:22:10 +0530

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
-import PdfPage from './PdfPage';
+import Pdfpage from './Pdfpage';
 import CareerGuide from './Carrierguide';
 import PdfLibrary from './Pdflibrary';
 import ContinueLearning from './ContinueLearning';
@@ -16,7 +16,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={isLoggedIn ? <DashboardPage /> : <Navigate to="/DashboardPage" />} />
-        <Route path="/Pdfpage" element={isLoggedIn ? <PdfPage /> : <Navigate to="/PdfPage" />} />
+        <Route path="/Pdfpage" element={isLoggedIn ? <Pdfpage /> : <Navigate to="/Pdfpage" />} />
         <Route path="/Carrierguide" element={isLoggedIn ? <CareerGuide /> : <Navigate to="/Carrierguide" />} />
         <Route path="/Pdflibrary" element={isLoggedIn ? <PdfLibrary /> : <Navigate to="/Pdflibrary" />} />
         <Route path="/ContinueLearning" element={isLoggedIn ? <ContinueLearning /> : <Navigate to="/ContinueLearning" />} />
